7183b27c0852c1e2cee1e08f196087d6bb22a570
Rithishaa <rithishaa14@gmail.com>
2026-08-21 22:48:09 +0800

    Add member 1 database schema

---
+++ schema.sql
@@ -0,0 +1,25 @@
+--User table (MEMBER 1: authentication & profile)
+CREATE TABLE IF NOT EXISTS User (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    bio TEXT
+);
+
+-- Shelf table (MEMBER 1: custom shelves)
+CREATE TABLE IF NOT EXISTS Shelf (
+    shelf_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    shelf_name TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE CASCADE
+);
+
+-- ShelfBook Table (MEMBER 1&2: Prevents duplicates in same shelf + Reading Status)
+CREATE TABLE IF NOT EXISTS ShelfBook (
+    shelf_id INTEGER NOT NULL,
+    book_id INTEGER NOT NULL,
+    reading_status TEXT CHECK (reading_status IN ('Want to Read', 'Currently Reading', 'Completed' )) DEFAULT 'Want to Read',
+    PRIMARY KEY (shelf_id, book_id),
+    FOREIGN KEY (shelf_id) REFERENCES Shelf(shelf_id) ON DELETE CASCADE
+);
