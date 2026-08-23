522ca03112a9055ce15a2d95f96cdd090687e902
Rithishaa <rithishaa14@gmail.com>
2026-08-23 18:41:36 +0800

    updated by adding register page code

--- app.py
+++ app.py
@@ -3,3 +3,32 @@ import sqlite3
 
 app = Flask(__name__)
 app.secret_key = 'mysecretkey'
+
+#connecting to database
+def connect_db() :
+    conn = sqlite3.connect('booksession.db')
+    conn.row_factory = sqlite3.Row 
+    return conn
+
+#register page
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        conn = connect_db()
+        cursor = conn.cursor(
+             'INSERT INTO User (username, email, password_hash) VALUES (?, ?, ?)',
+             (username, email, password)
+        )
+        conn.commit()
+        conn.close()
+
+        return redirect('/login')
+    return render_template('register.html')
+
+#login page
+
+        
