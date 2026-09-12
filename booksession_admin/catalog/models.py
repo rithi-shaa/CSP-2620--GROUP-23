e960abd9b34cd5c6f79690b5802c07b41aec1641
Rithishaa <rithishaa14@gmail.com>
2026-09-12 22:31:16 +0800

    add django admin project

---
+++ booksession_admin/catalog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    genre = models.CharField(max_length=100)
+    added_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
