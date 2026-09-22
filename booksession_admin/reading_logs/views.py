54b3ec201b4152edbc16661fc501db745699ddf7
Tavinya sri sangar <tavinyashankar@gmail.com>
2026-09-23 02:23:23 +0800

    Added reading log feature

--- booksession_admin/reading_logs/views.py
+++ booksession_admin/reading_logs/views.py
@@ -1,12 +1,4 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
-@login_required
-def reading_logs(request):
-    logs = ReadingLog.objects.filter(
-        user_id=request.user.id
-    ).select_related('book').order_by('-log_date')
-
-    return render(request, 'reading_logs.html', {
-        'logs': logs
-    })
+from .models import ReadingLog
