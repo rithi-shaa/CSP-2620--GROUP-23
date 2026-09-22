3987ca23bcc22cbe6041dcd2667a2f4856a27278
Tavinya sri sangar <tavinyashankar@gmail.com>
2026-09-23 02:50:40 +0800

    Added reading log feature

--- booksession_admin/reading_logs/forms.py
+++ booksession_admin/reading_logs/forms.py
@@ -1,3 +1,14 @@
 from django import forms
 
 from .models import ReadingLog
+
+class ReadingLogForm(forms.ModelForm):
+
+    class Meta:
+        model = ReadingLog
+
+        fields = [
+            'book',
+            'pages_read',
+            'log_date',
+        ]
