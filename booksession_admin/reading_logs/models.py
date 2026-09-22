e4b1e25d1b8fead0038165cacc5e3a5df6cc46ba
Tavinya sri sangar <tavinyashankar@gmail.com>
2026-09-23 02:21:30 +0800

    Added reading log feature

--- booksession_admin/reading_logs/models.py
+++ booksession_admin/reading_logs/models.py
@@ -8,3 +8,18 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+class ReadingLog(models.Model):
+    log_id = models.AutoField(primary_key=True)
+
+    pages_read = models.IntegerField()
+
+    log_date = models.DateField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        db_column='book_id'
+    )
