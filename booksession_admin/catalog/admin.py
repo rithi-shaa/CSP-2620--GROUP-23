fbc14f41012edeb3282053c45dc81ab259ab2136
Tavinya sri sangar <tavinyashankar@gmail.com>
2026-09-23 09:53:18 +0800

    Added reading log feature

--- booksession_admin/catalog/admin.py
+++ booksession_admin/catalog/admin.py
@@ -3,3 +3,31 @@ from .models import Book, UserProfile
 
 admin.site.register(Book)
 admin.site.register(UserProfile)
+
+from django.contrib import admin
+
+from .models import Book, ReadingLog
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        'book_id',
+        'title',
+    )
+
+
+@admin.register(ReadingLog)
+class ReadingLogAdmin(admin.ModelAdmin):
+    list_display = (
+        'log_id',
+        'book',
+        'user',
+        'pages_read',
+        'log_date',
+        'created_at',
+    )
+
+    list_filter = (
+        'log_date',
+    )
