8f802f239d27666c9fed20125c25ab68195297b6
Tavinya sri sangar <tavinyashankar@gmail.com>
2026-09-23 09:44:18 +0800

    Added reading log feature

--- booksession_admin/catalog/views.py
+++ booksession_admin/catalog/views.py
@@ -393,3 +393,24 @@ def delete_book(request, pk):
         print(f"Deleting book: {book.title}") # For testing purposes
         book.delete()
     return redirect('book_catalog')
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+from .models import ReadingLog
+
+
+@login_required
+def reading_logs(request):
+
+    logs = ReadingLog.objects.select_related('book').filter(
+        user=request.user
+    ).order_by('-log_date')
+
+    return render(
+        request,
+        'reading_logs/reading_log_list.html',
+        {
+            'logs': logs
+        }
+    )
