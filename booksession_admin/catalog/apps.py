b406324715b36ebcbce673ea58fb12310beef66a
Tavinya sri sangar <tavinyashankar@gmail.com>
2026-09-23 09:52:10 +0800

    Added reading log feature

--- booksession_admin/catalog/apps.py
+++ booksession_admin/catalog/apps.py
@@ -3,3 +3,10 @@ from django.apps import AppConfig
 
 class CatalogConfig(AppConfig):
     name = 'catalog'
+
+from django.apps import AppConfig
+
+
+class ReadingLogsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'reading_logs'
